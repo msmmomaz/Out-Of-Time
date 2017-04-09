2e0cd05238bfddb57515b397d328c0ef2615a1db
msmmomaz <msmmomaz@gmail.com>
2017-04-09 14:32:23 -0400

    Update Movement.js

--- Movement.js
+++ Movement.js
@@ -5,4 +5,10 @@
  function Update() {
      var move = Vector3(Input.GetAxis("Horizontal")*4, Input.GetAxis("Vertical")*4, 0);
      transform.position += move * speed * Time.deltaTime;
+
+      if (transform.position.y <= -4.5) {
+     transform.position = new Vector2(transform.position.x, -4.5f);
+ } else if (transform.position.y >= 4.5f) {
+     transform.position = new Vector2(transform.position.x, 4.5f);
+ }
  }
