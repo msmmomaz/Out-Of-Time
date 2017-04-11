44ab20a756e8f214968bcf54cc300e7414aaccdd
msmmomaz <msmmomaz@gmail.com>
2017-04-11 17:07:15 -0400

    Create Timer.cs

---
+++ Timer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+
+	[SerializeField]
+	public float CountdownFrom;
+	[SerializeField]
+	public Text CountdownText;
+
+	void Start() {
+		if (CountdownText == null) {
+			Debug.LogError("STATUS INDICATOR: No text object referenced!");
+		}
+	}
+	void Update()
+	{
+		float time = CountdownFrom - Time.timeSinceLevelLoad;
+		CountdownText.text = time.ToString("0:00") + "";
+
+		if (time <= 0f)
+		{
+			TimeUp();
+		}
+	}
+
+	void TimeUp()
+	{
+		Destroy(gameObject);
+	}
+}
