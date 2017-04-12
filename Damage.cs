0d5c9b8a874755c4ad40e1e37e7fef4a1156e924
msmmomaz <msmmomaz@gmail.com>
2017-04-12 15:48:09 -0400

    Create Damage.cs

---
+++ Damage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour 
+{
+	public bool isDamaging;
+	public float damage = 10;
+
+	private void OnTriggerStay2D(Collider2D col2D)
+		{
+		if (col2D.tag == "Player")
+			col2D.SendMessage((isDamaging)?"TakeDamage":"HealDamage",Time.deltaTime * damage);
+		}
+}
+
